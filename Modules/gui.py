3b6adb89cc7d744176194c014cd7ed760fab6804
Pavel Zhukov <48322035+Dimitryliss1@users.noreply.github.com>
2021-11-17 22:41:28 +0300

    Translate to Russian

--- Modules/gui.py
+++ Modules/gui.py
@@ -9,6 +9,7 @@ class GUI(Tk):
     p2: DoubleVar
     n: IntVar
     m: IntVar
+
     def __init__(self):
         super(GUI, self).__init__()
         self.title("Курсовая по ОЭВМ")
@@ -20,23 +21,28 @@ class GUI(Tk):
         self.mainloop()
 
     def draw(self):
-        Label(self, text="Select P1: ").grid(column=0, row=0, sticky=W)
+        Label(self, text="Выберите P1: ").grid(column=0, row=0, sticky=W)
         for cnt_col, i in enumerate([0.6, 0.8, 0.9]):
             Radiobutton(text=i, value=i, variable=self.p1).grid(row=0, column=cnt_col + 1, sticky=W)
 
-        Label(self, text="Select N: ").grid(column=0, row=1, sticky=W)
+        Label(self, text="Выберите N: ").grid(column=0, row=1, sticky=W)
         for cnt_col, i in enumerate([2, 5, 10]):
             Radiobutton(text=i, value=i, variable=self.n).grid(row=1, column=cnt_col + 1, sticky=W)
 
-        Label(self, text="Select M: ").grid(column=0, row=2, sticky=W)
+        Label(self, text="Выберите M: ").grid(column=0, row=2, sticky=W)
         for cnt_col, i in enumerate([4, 8, 16]):
             Radiobutton(text=i, value=i, variable=self.m).grid(row=2, column=cnt_col + 1, sticky=W)
 
-        Label(self, text="Select P2: ").grid(column=0, row=3, sticky=W)
+        Label(self, text="Выберите P2: ").grid(column=0, row=3, sticky=W)
         for cnt_col, i in enumerate([0.5, 0.7, 0.9]):
             Radiobutton(text=i, value=i, variable=self.p2).grid(row=3, column=cnt_col + 1, sticky=W)
 
-        Button(self, text='Simulate!', command=self.simulate).grid(row=4, column=0, columnspan=4)
+        Label(self, text="P1 - вероятность регистровой адресации для операнда\n"
+                         "N - время обращения к памяти в тактах\n"
+                         "M - время вычисления для команд второго типа\n"
+                         "P2 - вероятность, что текущая команда является командой первого типа", justify=CENTER).grid(row=4, column=0, columnspan=4)
+        Button(self, text='Симулировать!', command=self.simulate).grid(row=5, column=0, columnspan=4)
+
 
     def simulate(self):
         if not self.p1.get() or not self.p2.get() or not self.m.get() or not self.n.get():
@@ -51,8 +57,7 @@ class GUI(Tk):
             ys[i] = ys[i] / xs[i]
         plt.plot(xs, ys)
         plt.grid()
-        plt.xlabel("N of operations")
-        plt.ylabel("Mean ticks per operation")
-        plt.title(
-            f"p1 = {self.p1.get()}, n = {self.n.get()}, m = {self.m.get()}, p2 = {self.p2.get()}")
+        plt.xlabel("Количество посчитанных операций")
+        plt.ylabel("Среднее количество тактов на операцию")
+        plt.title(f"p1 = {self.p1.get()}, n = {self.n.get()}, m = {self.m.get()}, p2 = {self.p2.get()}")
         plt.show()
